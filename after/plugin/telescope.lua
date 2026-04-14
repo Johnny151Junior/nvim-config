b2af20268cc52c97a085eda7caa25c2809a40073
Johnny151Junior <johnnysyldev@gmail.com>
2026-04-14 16:34:28 +0700

    telescope config invalid variable use

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local buildin = require('telescope.builtin')
 vim.keymap.set("n",'<leader>pf',buildin.find_files,{})
 vim.keymap.set("n",'<C-p>',buildin.git_files,{})
-vim.keymap.set("n",'<leader>ps',function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+vim.keymap.set("n",'<leader>ps', function()
+	buildin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
